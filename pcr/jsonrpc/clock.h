05cab6737810c3b3b398726e3e87ae11cc9c1e22
Ziyu Chen <zychenllp@gmail.com>
2026-04-27 21:17:40 -0700

    add win build for jsonrpc

--- pcr/jsonrpc/clock.h
+++ pcr/jsonrpc/clock.h
@@ -1,16 +1,26 @@
 #pragma once
 
 #include <cstdint>
-#include <ctime>
+// #include <ctime>
+#include <chrono>
 
 namespace pcr::jsonrpc {
 
 // Monotonic clock in ns.
-inline std::uint64_t now_ns() noexcept {
-    timespec ts{};
-    ::clock_gettime(CLOCK_MONOTONIC, &ts);
-    return static_cast<std::uint64_t>(ts.tv_sec) * 1000000000ull +
-           static_cast<std::uint64_t>(ts.tv_nsec);
+// inline std::uint64_t now_ns() noexcept 
+// {
+//     timespec ts{};
+//     ::clock_gettime(CLOCK_MONOTONIC, &ts);
+//     return static_cast<std::uint64_t>(ts.tv_sec) * 1000000000ull +
+//            static_cast<std::uint64_t>(ts.tv_nsec);
+// }
+
+inline std::uint64_t now_ns() noexcept 
+{
+    auto now = std::chrono::steady_clock::now();
+    return static_cast<std::uint64_t>(
+        std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch()).count()
+    );
 }
 
 } // namespace pcr::jsonrpc
